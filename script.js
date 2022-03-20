7a267d35cf5724e745bf6324d1249696d7e7c301
kowai-onigiri <alexischambers627@gmail.com>
2022-03-20 14:48:57 -0400

    add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// rock paper scissors
+// computer player
+
+let computerChoice = Math.floor(Math.random()*3)+1;
+
+function computerPlayer(computerChoice) {
+    if (computerChoice == 1) {
+        return "rock";
+    }
+    else if (computerChoice == 2) {
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
+}
+
+// human player
+
