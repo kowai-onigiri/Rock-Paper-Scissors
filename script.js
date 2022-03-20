257966dd9f7b9039f4014be6621d8813f1a8c561
kowai-onigiri <alexischambers627@gmail.com>
2022-03-20 15:23:19 -0400

    finish game functions

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-// computer player
+//game play
+function playRound(humanPlayer, computerPlayer) {
 
-let computerChoice = Math.floor(Math.random()*3)+1;
+    if (humanPlayer == computerPlayer) {
+        console.log("It's a tie!");
+    }
 
-function computerPlayer(computerChoice) {
-    if (computerChoice == 1) {
-        return "rock";
+    else if (humanPlayer == "rock") {
+        if (computerPlayer == "scissors") {
+            console.log("You win!");
+        }
+        else if (computerPlayer == "paper") {
+            console.log("You lose!");
+        }
     }
-    else if (computerChoice == 2) {
-        return "paper";
+
+    else if (humanPlayer == "paper") {
+        if (computerPlayer == "rock") {
+            console.log("You win!");
+        }
+        else if (computerPlayer == "scissors") {
+            console.log("You lose!");
+        }
     }
-    else {
-        return "scissors"
+
+    else if (humanPlayer == "scissors") {
+        if (computerPlayer == "paper") {
+            console.log("You win!");
+        }
+        else if (computerPlayer == "rock") {
+            console.log("You lose!")
+        }
     }
 }
 
-// human player
+// game loop for five turns
 
+function game() {
+    for (i = 0; i < 5; i++) {
+
+        //computer choice
+        const computerRand = Math.floor(Math.random()*3)+1;
+
+        function computerChoice(computerRand) {
+            if (computerRand == 1) {
+                return "rock";
+            }
+            else if (computerRand == 2) {
+                return "paper";
+            }
+            else {
+                return "scissors";
+            }
+        }
+
+        const computerPlayer = computerChoice(computerRand);
+        
+        // human choice
+        const humanPlayer = prompt("Choose rock, paper, or scissors and write it below:");
+
+        //call gameplay
+        console.log(playRound(humanPlayer, computerPlayer));
+    }
+}
+
+console.log(game());
